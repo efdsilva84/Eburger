7f49d7e3c003caabb74eed29863680df7d2b1a80
ed <efdsilva84@gmail.com>
2025-03-28 22:21:06 -0400

    first commit

---
+++ src/app/services/eburger.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { HttpHeaders } from '@angular/common/http';
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    Authorization: 'Admin 123456'
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EburgerService {
+
+  constructor(private api: ApiService) { }
+
+  getEburgers() {
+    return this.api.get('api/eburger/eburger_txt', false, httpOptions);
+  }
+}
