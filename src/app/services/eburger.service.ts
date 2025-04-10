18584a6c5ff8fe831940d48284f973032b1e4b6c
ed <efdsilva84@gmail.com>
2025-04-09 22:04:12 -0400

    correção url para deploy produção

--- src/app/services/eburger.service.ts
+++ src/app/services/eburger.service.ts
@@ -3,8 +3,8 @@ import { ApiService } from './api.service';
 import { HttpHeaders } from '@angular/common/http';
 const httpOptions = {
   headers: new HttpHeaders({
-    'Content-Type': 'application/json',
-    Authorization: 'Admin 123456'
+    'Content-Type': 'application/json'
+
   })
 };
 
@@ -16,9 +16,9 @@ export class EburgerService {
   constructor(private api: ApiService) { }
 
   getEburgers() {
-    return this.api.get('api/eburger/eburger_txt', false, httpOptions);
+    return this.api.get('eburger/eburger_txt', false, httpOptions);
   }
   getItemMenu() {
-    return this.api.get('api/eburger/itens_mn', false, httpOptions);
+    return this.api.get('eburger/itens_mn', false, httpOptions);
   }
 }
